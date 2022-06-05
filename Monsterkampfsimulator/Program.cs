1aa76e18713390990f5b8f64ad1349ff909ce9f5
T0wby <tobiasnuehlen@gmx.de>
2022-06-05 18:25:45 +0200

    -Formatting changes

--- Monsterkampfsimulator/Program.cs
+++ Monsterkampfsimulator/Program.cs
@@ -13,78 +13,78 @@ namespace Monsterkampfsimulator
             do
             {
                 // Build all sounds that are used during the game
-            Sound.BuildSound();
+                Sound.BuildSound();
 
-            // Starting with setting the console size if possible, writing a text and looping the main song
-            Messages.WelcomeMessage(Sound.MenuStartSound);
+                // Starting with setting the console size if possible, writing a text and looping the main song
+                Messages.WelcomeMessage(Sound.MenuStartSound);
 
-            // While loop which ends as soon as we get the correct input
-            Messages.WaitingForSKey();
+                // While loop which ends as soon as we get the correct input
+                Messages.WaitingForSKey();
 
-            // ASCII messages for the first display of available fighters
-            Messages.DisplayFightersOne();
+                // ASCII messages for the first display of available fighters
+                Messages.DisplayFightersOne();
 
-            // Getting the User input and setting the first fighter
-            FightLogic.GetFighterOneInput();
+                // Getting the User input and setting the first fighter
+                FightLogic.GetFighterOneInput();
 
-            Console.Clear();
+                Console.Clear();
 
-            // Getting the User input and setting the second fighter
-            FightLogic.GetFighterTwoInput();
+                // Getting the User input and setting the second fighter
+                FightLogic.GetFighterTwoInput();
 
-            Console.Clear();
+                Console.Clear();
 
-            // Now we start asking the user to enter the stats for each of the two fighters
-            Messages.ExplanationMessage();
+                // Now we start asking the user to enter the stats for each of the two fighters
+                Messages.ExplanationMessage();
 
-            // Function that gets all the input for the first fighter
-            FightLogic.GetFighterOneStats();
+                // Function that gets all the input for the first fighter
+                FightLogic.GetFighterOneStats();
 
-            // Getting the speed of the first chosen fighter and then setting the variable to use it later as a comparison in GetFighterTwoStats()
-            FightLogic.SettingCompareSpeed();
+                // Getting the speed of the first chosen fighter and then setting the variable to use it later as a comparison in GetFighterTwoStats()
+                FightLogic.SettingCompareSpeed();
 
-            Console.Clear();
+                Console.Clear();
 
-            // Showing the selected stats for the first fighter
-            FightLogic.ShowStatsFighterOne();
+                // Showing the selected stats for the first fighter
+                FightLogic.ShowStatsFighterOne();
 
-            // Message between the collection of the stats of both fighters
-            Messages.TransitionMessage();
+                // Message between the collection of the stats of both fighters
+                Messages.TransitionMessage();
 
-            // Created a private function that gets all the input for the second fighter to try and keep the code more organized
-            FightLogic.GetFighterTwoStats();
+                // Created a private function that gets all the input for the second fighter to try and keep the code more organized
+                FightLogic.GetFighterTwoStats();
 
-            // Deciding who starts the fight and then setting the bool which is later used to check which Monster gets the first Attack
-            FightLogic.WhoStartsFight();
+                // Deciding who starts the fight and then setting the bool which is later used to check which Monster gets the first Attack
+                FightLogic.WhoStartsFight();
 
-            Console.Clear();
+                Console.Clear();
 
-            // Message ending the attribute collection
-            Messages.AttributeEndMessage();
+                // Message ending the attribute collection
+                Messages.AttributeEndMessage();
 
-            // Visualization of the final attributes
-            FightLogic.ShowStatsBothFighters();
+                // Visualization of the final attributes
+                FightLogic.ShowStatsBothFighters();
 
-            // Waiting for an input to continue the code in order to not overwhelm the user
-            Console.ReadKey();
+                // Waiting for an input to continue the code in order to not overwhelm the user
+                Console.ReadKey();
 
-            // Starting sound for the fight in Async and Sync to make sure the sound is getting loaded properly and played fully as well
-            Messages.StartFightMessageAndSound(Sound.FightStartSound);
+                // Starting sound for the fight in Async and Sync to make sure the sound is getting loaded properly and played fully as well
+                Messages.StartFightMessageAndSound(Sound.FightStartSound);
 
-            // Starting the fight by checking which fighters were chosen by the user
-            FightLogic.StartFight();
+                // Starting the fight by checking which fighters were chosen by the user
+                FightLogic.StartFight();
 
-            // Play sound at the end
-            Sound.PlayEndSound();
+                // Play sound at the end
+                Sound.PlayEndSound();
 
-            // Delay to Display end screen before we proceed
-            Thread.Sleep(10000);
+                // Delay to Display end screen before we proceed
+                Thread.Sleep(10000);
 
-            // Credits
-            Messages.Credits();
+                // Credits
+                Messages.Credits();
 
-            // Last part of our game loop. Player gets the choice to end the game or play another round
-            FightLogic.PlayAnother();
+                // Last part of our game loop. Player gets the choice to end the game or play another round
+                FightLogic.PlayAnother();
 
             } while (playAnother);
         }
